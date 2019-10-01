dc354df1ce39b39cbee8ac82441927f4992512e1
pattarapon songarsa <tunmypro@hotmail.co.th>
2019-10-01 15:54:11 +0700

    เทส

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CsManager.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "เทส";
 
             return View();
         }
