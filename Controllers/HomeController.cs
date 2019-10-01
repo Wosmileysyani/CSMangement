950147e9fc4d7eb4edce58f690ed671d581faab1
pattarapon songarsa <tunmypro@hotmail.co.th>
2019-10-01 16:00:55 +0700

    เทสสอง

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CsManager.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "เทสสอง";
 
             return View();
         }
